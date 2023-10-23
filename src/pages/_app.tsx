4b974cef57491ed8394af5a678c2ac0c5b1d576e
r2ditto <ryan.ebro@yahoo.com>
2023-10-22 23:57:47 -0400

    amplify configure

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+
+import { Amplify } from "aws-amplify";
+import awsExports from "../aws-exports";
+
+Amplify.configure(awsExports);
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
